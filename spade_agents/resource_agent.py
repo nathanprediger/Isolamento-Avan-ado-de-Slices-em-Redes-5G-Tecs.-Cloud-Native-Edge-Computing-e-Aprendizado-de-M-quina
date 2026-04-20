650dddf8b953334c6bab34fb7fa78dc4fa40e572
Nathan Rambo <nathanpredigerrambo@gmail.com>
2026-04-20 12:54:06 -0300

    [ADICIONADO] Funções de update de limites de banda e memória

--- spade_agents/resource_agent.py
+++ spade_agents/resource_agent.py
@@ -52,7 +52,7 @@ class ResourceAgent(Agent):
                         patch ={
                             "spec": {
                                 "containers": [{
-                                    "name": pod_name,  
+                                    "name": upf_name,  
                                         "resources": {
                                             "limits": {
                                                 "cpu": new_cpu
@@ -68,8 +68,70 @@ class ResourceAgent(Agent):
             except Exception as e:
                 print(f"[ERROR] Failed to update CPU for UPF {upf_name}: {e}")
 
+        def update_pod_bandwidth(self, upf_name, new_bandwidth):
+            try:
+                pods = self.v1.list_namespaced_pod(namespace=NAMESPACE, label_selector=f"app={upf_name}")
+                if pods.items:
+                    for pod in pods.items:
+                        pod_name = pod.metadata.name
+                        patch = {
+                            "metadata": {
+                                "annotations" : {
+                                    "qos.porjectcalico.org/ingressBandwidth":new_bandwidth,
+                                    "qos.projectcalico.org/egressBandwidth":new_bandwidth
+                                }
+                            }
+                        }
+                    self.v1.patch_namespaced_pod(name=pod_name, namespace=NAMESPACE, body=patch)
+                    print(f"[SUCCESS] Updated BANDWIDTH for pod {pod_name} to {new_bandwidth}")
+                else:
+                    print(f"[ERROR] No pods found for UPF {upf_name}")
+            except Exception as e:
+                print(f"[ERROR] Failed to update BANDWIDTH for UPF {upf_name}: {e}")
+        
+        def update_pod_memory(self, upf_name, new_memory):
+             try:
+                # Fetch the corresponding pod
+                pods = self.v1.list_namespaced_pod(namespace=NAMESPACE, label_selector=f"app={upf_name}")
+                if pods.items:
+                    for pod in pods.items:
+                        # Update the CPU resource request/limit
+                        pod_name = pod.metadata.name
+                        # Create a patch to update the CPU resources
+                        patch ={
+                            "spec": {
+                                "containers": [{
+                                    "name": upf_name,  
+                                        "resources": {
+                                            "limits": {
+                                                "memory": new_memory
+                                            }
+                                        }
+                                }]
+                            }
+                        }
+                        self.v1.patch_namespaced_pod(name=pod_name, namespace=NAMESPACE, body=patch)
+                        print(f"[SUCCESS] Updated CPU for pod {pod_name} to {new_memory}")
+                else:
+                    print(f"[ERROR] No pods found for UPF {upf_name}")
+            except Exception as e:
+                print(f"[ERROR] Failed to update CPU for UPF {upf_name}: {e}")
 
     async def setup(self):
         print("ResourceAgent starting...")
         self.add_behaviour(self.ResourceBehavior())
         return await super().setup()
+
+async def main():
+    resource_agent = ResourceAgent("resource_agent@localhost", "password")
+    await resource_agent.start()
+    print("ResourceAgent is running...")
+    try:
+        while resource_agent.is_alive():
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping ResourceAgent...")
+    await resource_agent.stop()
+
+if __name__ == "__main__":
+    spade.run(main(), embedded_xmpp_server=True)
